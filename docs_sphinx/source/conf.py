d192ebf291a98dee49eb4e5a659b9571a4d41436
An Phan <anphan@aitomatic.com>
2025-07-20 14:51:19 -0700

    doc: add Sphinx documentation

---
+++ docs_sphinx/source/conf.py
@@ -0,0 +1,136 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ML Systems Evaluation Framework'
+copyright = '2025, Phan Hong An'
+author = 'Phan Hong An'
+release = '0.1.0'
+version = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
+]
+
+# Napoleon settings for Google-style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'requests': ('https://requests.readthedocs.io/en/stable/', None),
+    'click': ('https://click.palletsprojects.com/en/8.1.x/', None),
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# Theme options
+html_theme_options = {
+    'navigation_depth': 4,
+    'titles_only': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': '#2980B9',
+}
+
+# HTML context
+html_context = {
+    'display_github': True,
+    'github_user': 'phanhongan',
+    'github_repo': 'ml-systems-evaluation',
+    'github_version': 'main',
+    'conf_py_path': '/docs_sphinx/source/',
+    'source_suffix': '.rst',
+}
+
+# -- Options for LaTeX output ------------------------------------------------
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '11pt',
+    'figure_align': 'htbp',
+}
+
+# -- Options for manual page output ------------------------------------------
+man_pages = [
+    ('index', 'ml-systems-evaluation', 'ML Systems Evaluation Framework Documentation',
+     ['Phan Hong An'], 1)
+]
+
+# -- Options for Texinfo output ----------------------------------------------
+texinfo_documents = [
+    ('index', 'ml-systems-evaluation', 'ML Systems Evaluation Framework Documentation',
+     'Phan Hong An', 'ml-systems-evaluation', 'A comprehensive framework for evaluating ML systems.',
+     'Miscellaneous'),
+]
+
+# -- Extension configuration -------------------------------------------------
+todo_include_todos = True
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# The master toctree document.
+master_doc = 'index'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
